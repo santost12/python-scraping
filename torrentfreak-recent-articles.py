c4ac10a7104b1b9832a8cd0d917d2a4088f9b342
Tyler <45154554+santost12@users.noreply.github.com>
2022-11-06 19:54:29 -0500

    Create torrentfreak-recent-articles.py

---
+++ torrentfreak-recent-articles.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import re
+import requests
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0'}
+url = "https://torrentfreak.com/"
+
+page = requests.get(url, headers=headers)
+entire_page = page.text
+
+urls = re.findall(r"\"https://torrentfreak\.com/.*?/\"", entire_page)
+
+for url in urls:
+    filter = re.search(r"https://torrentfreak.com\/(page|category|wp-json|contact|subscriptions|copyright|privacy|about).*",url)
+    if filter != None:
+        pass
+    else:
+        print(url)
