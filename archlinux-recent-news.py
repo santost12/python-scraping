83e9c51b3683ba31e937f15a7594ee3283901c6a
santost12 <45154554+santost12@users.noreply.github.com>
2022-11-29 17:12:11 -0500

    add archlinux-recent-news.py

---
+++ archlinux-recent-news.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from bs4 import BeautifulSoup
+import requests
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0'}
+url = "https://archlinux.org/"
+
+page = requests.get(url, headers=headers)
+entire_page = page.text
+
+soup = BeautifulSoup(entire_page, 'html.parser')
+news_div = soup.find_all("div", id="news")
+
+for elements in news_div:
+    h4_elements = elements.find_all("h4")
+
+    for h4 in h4_elements:
+        links = h4.find_all("a",href=True)
+
+        for link in links:
+            print(link.get_text())
+            print("https://archlinux.org" + link.get("href"))
+            print("")
