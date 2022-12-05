82cc0e259609a8ecd873f60bf6a63e5377a798db
santost12 <45154554+santost12@users.noreply.github.com>
2022-12-04 22:33:05 -0500

    add print-all-urls.py

---
+++ print-all-urls.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+import requests
+
+if len(sys.argv) != 2:
+    print("Error: Too many or too few arguments\n")
+    print("Example: print-all-urls.py \"https://www.google.com/\"")
+    exit()
+
+url = sys.argv[1]
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0'}
+page = requests.get(url, headers=headers)
+entire_page = page.text
+
+https_urls = re.findall(r"\"https://.*?\"", entire_page)
+http_urls = re.findall(r"\"http://.*?\"", entire_page)
+
+all_urls = http_urls + https_urls
+counter = 0
+
+for url in all_urls:
+    print(url)
+    counter = counter + 1
+
+print("\nTotal number of URLs found: " + str(counter))
