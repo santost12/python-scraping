9a18b49f84de5d254c623e8cea5f61a28de23631
santost12 <45154554+santost12@users.noreply.github.com>
2023-02-24 15:11:16 -0500

    add gentoo-security-news.py

---
+++ gentoo-security-news.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0'}
+url = "https://security.gentoo.org/"
+
+page = requests.get(url, headers=headers)
+entire_page = page.text
+
+soup = BeautifulSoup(entire_page, 'html.parser')
+news_table = soup.find_all("table", {"class": "table table-striped"})
+
+for elements in news_table:
+    tr_elements = elements.find_all("tr")
+
+    for th in tr_elements[1:]:
+        print(th.get_text())
